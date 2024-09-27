252e7de68e166c912d8ef42a9b237e7cb08b8bb5
Moon <moon.kang@inka.co.kr>
2024-09-27 10:46:09 +0900

    add placeholders for images

--- components/RoomList/index.tsx
+++ components/RoomList/index.tsx
@@ -1,11 +1,21 @@
 import { RoomType } from '@/interface'
-
+import Image from 'next/image'
 export function RoomItem({ room }: { room: RoomType }) {
   return (
     <div key={room.id}>
-      <img src={room?.images?.[0]} alt={room.title} className="rounded-md w-full h-auto object-fit" />
+      <Image
+        src={room?.images?.[0]}
+        alt={room.title}
+        className="rounded-md w-full h-auto object-fit"
+        width={300}
+        height={300}
+        placeholder="blur"
+        blurDataURL={'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPknzWrHgADxwHERIJN5gAAAABJRU5ErkJggg=='}
+      />
       <div className="mt-2 font-semibold text-sm">{room.title}</div>
-      <span className="text-xs px-2 py-1 rounded-full bg-black text-white mt-1">{room.category}</span>
+      <span className="text-xs px-2 py-1 rounded-full bg-black text-white mt-1">
+        {room.category}
+      </span>
       <div className="mt-1 text-gray-400 text-sm">{room.address}</div>
       <div className="mt-1 text-sm">
         {room?.price?.toLocaleString()}원 <span className="text-gray-500"> /박</span>
@@ -21,4 +31,3 @@ export function GridLayout({ children }: { children: React.ReactNode }) {
     </div>
   )
 }
-
