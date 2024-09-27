7f13668f5ae3fa2f67f290891d4c517f4277c647
Moon <moon.kang@inka.co.kr>
2024-09-27 09:51:25 +0900

    fix name bug for infinite scroll

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -7,22 +7,28 @@ export async function GET(req: Request) {
   const limit = searchParams.get('limit') as string
 
   if (page) {
-    // 무한 스크롤 페이지 처리
+    // 무한 스크롤 로직
     const count = await prisma.room.count()
     const skipPage = parseInt(page) - 1
-    const rooms = await prisma.room.findMany({ orderBy: { id: 'asc' }, skip: skipPage * parseInt(limit), take: parseInt(limit) });
-
-    return NextResponse.json({
-      data: rooms,
-      totalCount: count,
-      page: parseInt(page),
-      totalPages: Math.ceil(count / parseInt(limit)),
-    }, {
-      status: 200,
+    const rooms = await prisma.room.findMany({
+      orderBy: { id: 'asc' },
+      take: parseInt(limit),
+      skip: skipPage * parseInt(limit),
     })
+
+    return NextResponse.json(
+      {
+        page: parseInt(page),
+        data: rooms,
+        totalCount: count,
+        totalPage: Math.ceil(count / parseInt(limit)),
+      },
+      { status: 200 },
+    )
   }
 
   const data = await prisma.room.findMany()
+
   return NextResponse.json(data, {
     status: 200,
   })
--- hooks/useIntersectionObserver.ts
+++ hooks/useIntersectionObserver.ts
@@ -1,20 +1,29 @@
-import { RefObject, useEffect, useState } from 'react'
+import { RefObject, useState, useEffect } from 'react'
 
-function useIntersectionObserver(ref: RefObject<HTMLElement>, {threshold = 0.1, root = null, rootMargin = '0px'}) {
-  const [entry, setEntry] = useState<IntersectionObserverEntry | null>(null)
-  const updateEntry = ([entries]: IntersectionObserverEntry[]):void => {
-    setEntry(entries)
+function useIntersectionObserver(
+  elementRef: RefObject<Element>,
+  { threshold = 0.1, root = null, rootMargin = '0%' },
+) {
+  const [entry, setEntry] = useState<IntersectionObserverEntry>()
+
+  const updateEntry = ([entry]: IntersectionObserverEntry[]): void => {
+    setEntry(entry)
   }
+
   useEffect(() => {
-    const node = ref?.current
+    const node = elementRef?.current
     const hasIOSupport = !!window.IntersectionObserver
-    if (!hasIOSupport || !node) return
-    const observerParams = {threshold, root, rootMargin}
+
+    if (!node || !hasIOSupport) return
+
+    const observerParams = { threshold, root, rootMargin }
     const observer = new IntersectionObserver(updateEntry, observerParams)
 
     observer.observe(node)
+
     return () => observer.disconnect()
-  }, [ref.current, root, rootMargin, JSON.stringify(threshold)])
+  }, [elementRef?.current, root, rootMargin, JSON.stringify(threshold)])
+
   return entry
 }
 
