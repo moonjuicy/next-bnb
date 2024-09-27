707839ba62974066f276cc0c83d8ed9d4d03cafa
Moon <moon.kang@inka.co.kr>
2024-09-27 12:54:22 +0900

    add map to each room's detail page

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,5 +1,5 @@
 import Map from '@/components/Map'
-import SelectedRoom from './SelectedRoom'
+import SelectedRoom from '../../components/Map/SelectedRoom'
 export default function MapPage() {
 
   return (
